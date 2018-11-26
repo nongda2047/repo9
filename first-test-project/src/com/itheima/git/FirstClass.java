3b726c75780dbda7cb0c2f858518ceaf214b36a2
zhangsan <zhangsan@qq.com>
2018-11-26 21:29:31 +0800

    333

--- first-test-project/src/com/itheima/git/FirstClass.java
+++ first-test-project/src/com/itheima/git/FirstClass.java
@@ -3,8 +3,10 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
     }
     public void test(){
+        System.out.println("aaa");
         System.out.println("aaa");
     }
 }
